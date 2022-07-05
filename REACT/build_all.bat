b1566817fdf93d7872529972e7b92e1d19a8a0c2
Alexander <alex-xp@list.ru>
2022-07-05 22:56:42 +0500

    build 05.07.2022-2256

---
+++ REACT/build_all.bat
@@ -0,0 +1,36 @@
+@ECHO OFF
+
+@SET ROOT_DIR=%~dp0
+@cd %ROOT_DIR%
+
+ECHO "CLEAR ALL DATA"
+cd ..\SERVER\public
+
+call RD /S/Q admin
+mkdir admin
+
+call RD /S/Q client
+mkdir client
+
+cd %ROOT_DIR%
+
+
+ECHO "************************ COMPILE ADMIN\001-login ************************"
+cd ADMIN\001-login\.build
+call compile-prod.bat
+cd %ROOT_DIR%
+
+
+ECHO "************************ COMPILE ADMIN\002-admin_panel ************************"
+cd ADMIN\002-admin_panel\.build
+call compile-prod.bat
+cd %ROOT_DIR%
+
+
+
+ECHO "************************ COMPILE CLIENTS\001-index ************************"
+cd CLIENTS\001-index\.build
+call compile-prod.bat
+cd %ROOT_DIR%
+
+pause
